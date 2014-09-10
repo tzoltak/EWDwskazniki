50a55cc4620b02477c6093a4abd78a45caf72201
g.golonka <g.golonka@ibe.edu.pl>
2014-09-10 12:50:02 +0200

    Poprawki pakietu. Dodanie dokumentacji.

--- R/diagnostykaWielomianow.R
+++ R/diagnostykaWielomianow.R
@@ -10,112 +10,105 @@
 #' @param wykresyFun funkcja rysująca wykresy. Domyślnie jest to \code{png}.
 #' @param wykresyParam lista parametrów funkcji rysującej wykresy.
 #' @param przew_nparPar lista z parametrami funkcji \code{\link{przew_npar}}.
-#' @param smooothScatterPar lista z parametrami funkcji \code{\link[utils]{smoothScatter}}.
+#' @param smooothScatterPar lista z parametrami funkcji \code{\link[graphics]{smoothScatter}}.
 #' @return funkcja zwraca listę z obliczeniami dla modelu.
+#' @import dplyr
+#' @import EWDogolny
+#' @import lme4
 #' @export
-diagnostyka_wielomianow <- function(model, zmienna, zmiennaGr = NULL,
+diagnostyka_wielomianow <- function(model, zmienna, zmiennaGr = NULL, 
                                     folderWykresy = NULL, wykresyFun = png, wykresyParam = NULL,
                                     przew_nparPar = NULL, smooothScatterPar = NULL){
+  
   if( class(model) != "lmerMod" & class(model) != "lm" ) {
     stop("Obiekt model nie jest klasy 'lm' ani 'lmerMod'.")
     return(invisible(FALSE))
   }
-
+  
   frame = modelFrame(model)
-  zmienna = wyciagnij_nazwe_zmiennej(zmienna)
+  zmiennaOrg = wyciagnij_nazwe_zmiennej(zmienna)
+  zmienna = colnames(model.matrix(model))[ grepl(paste0("^", zmiennaOrg, "$|^poly[(]", zmiennaOrg,", [[:digit:]]+(|, raw = TRUE)[)]1$") , colnames(model.matrix(model)))]
+  
   zmiennaGr = wyciagnij_nazwe_zmiennej(zmiennaGr, czyJednaZmienna = FALSE)
   zmiennaZalezna = all.vars(attributes(modelFrame(model))$terms)[1]
-
-  isPoly = any(indPoly <- grepl("^poly[(]", colnames(frame)))
-  if(isPoly){
-    zmiennaX = colnames(model.matrix(model))[ grepl(paste0("^poly[(]", zmienna,", [[:digit:]]+(|, raw = TRUE)[)]1$"), colnames(model.matrix(model)))]
-  }
-
-  if(   (!isPoly & !any(ktoraZmienna <- c(zmienna                                                 , zmiennaGr) %in% colnames(frame)))
-      | ( isPoly & !any(ktoraZmienna <- c(any(grepl(paste0("^poly[(]", zmienna), colnames(frame))), zmiennaGr) %in% colnames(frame)))
+  
+  if( (!any( ktoraZmienna <- c(zmienna, zmiennaGr) %in% colnames(model.matrix(model)))) 
   ){
     stop("Model został zbudowany na danych, które nie zawierają kolumn: ", c(zmienna, zmiennaGr)[!ktoraZmienna])
   }
-
-  grupowanie = daj_grupowanie(frame, zmiennaGr)
-
-  if( is.null(grupowanie) | nrow(grupowanie) != 0 ){
+  
+  if(is.null(zmiennaGr)){
+    grupowanie = NULL
+  } else{
+    grupowanie = expand.grid(sapply(frame[, zmiennaGr], unique))
+  }
+  
+  if( !is.null(grupowanie) ){
     grupowanieTab = cbind(grupowanie, grupa = 1:nrow(grupowanie))
-    tabGr = data.frame(frame[, c(zmiennaGr)])
+    tabGr = data.frame( frame[, c(zmiennaGr) ])
     tabGr = suppressMessages(inner_join(tabGr, grupowanieTab))
   }
-
+  
   linkTest = numeric(max(1, nrow(grupowanie)))
   monotonicznosc = logical(max(1, nrow(grupowanie)))
   for(indeksGrupowania in 1:max(1, nrow(grupowanie))){
-    if( is.null(grupowanie) | nrow(grupowanie) == 0 ){
+    
+    if( is.null(grupowanie) ){
       grupa = rep(TRUE, nrow(frame))
-    } else {
-      tabGr$grupa == indeksGrupowania
+    }else{
+      grupa = tabGr$grupa == indeksGrupowania
     }
-
-    message(paste0( apply(grupowanie[indeksGrupowania, ], 2, as.character), collapse=" " ))
-    wielomian = wielomian_lmer(model, zmienna, zmiennaGr, grupa, grupowanie, indeksGrupowania, nsiatka=0, czyTylkoGrupa = TRUE)
-    temp      = przewidywanie_lmer(model, wielomian, grupa, zmienna)
-
+    
+    # if(!is.null(grupowanie)) message("\n", paste0(apply(grupowanie[indeksGrupowania,], 2, as.character),collapse=" " ))
+    wielomian = wielomian_lmer(model, zmiennaOrg, zmiennaGr, grupa, grupowanie, indeksGrupowania, nsiatka=0, czyTylkoGrupa = TRUE)
+    temp      = przewidywanie_lmer(model, wielomian, grupa, zmiennaOrg)
+    
     wartosciZmiennejZaleznej = frame[, zmiennaZalezna]
     if( length(unique(wartosciZmiennejZaleznej[grupa])) > 100){
-      #       if(is.null(przew_nparPar)){
-      #         przew_nparPar = list()
-      #       }
-      #       przew_nparPar$formula = as.formula(paste0("reszty~", zmienna))
-      #       przew_nparPar$data = temp
-      przewNPar = przew_npar_rpr("reszty", zmienna, dane = temp, przew_nparPar)
-      #       przewNPar = do.call(przew_npar, przew_nparPar)
+      przewNPar = przew_npar_rpr("reszty", zmiennaOrg, dane = temp, przew_nparPar)
     } else {
-      przewNPar = regr_pierw_rodz("reszty", zmienna, dane = temp)
+      przewNPar = regr_pierw_rodz("reszty", zmiennaOrg, dane = temp)
     }
-
+    
     ## Rysowanie wykresów
     if( !is.null(folderWykresy) ){
       czescPliku = paste0(apply(grupowanie[indeksGrupowania, ], 2, as.character), collapse="_")
       plik = paste0(folderWykresy, "wykres_", czescPliku, ".png")
-
+      
       if(is.null(wykresyParam)){
         wykresyParam = list(height=1004, width=1004, pointsize=12, res=150)
       }
       wykresyParam$filename = plik
       do.call(wykresyFun, wykresyParam)
     }
-
+    
     if(is.null(smooothScatterPar)){
       smooothScatterPar = list(nbin=256, colramp=colorRampPalette(c("white", blues9)))
-      smooothScatterPar$bandwidth = c((max(temp[, zmienna]) - min(temp[, zmienna])) / 256, (max(temp$reszty) - min(temp$reszty)) / 256)
-
-      xx = if(isPoly){
-        model.matrix(model)[, zmiennaX]
-      } else{
-        frame[, zmienna]
-      }
-      smooothScatterPar$xlim = range(xx, na.rm = TRUE)
+      smooothScatterPar$bandwidth = c((max(temp[, zmiennaOrg]) - min(temp[, zmiennaOrg])) / 256, (max(temp$reszty) - min(temp$reszty)) / 256)
+      smooothScatterPar$xlim = range(model.matrix(model)[, zmienna], na.rm = TRUE)
       smooothScatterPar$ylim = range(frame[, zmiennaZalezna], na.rm = TRUE)
     }
-
-    smooothScatterPar$x = data.frame(x = temp[, zmienna] , y = temp$reszty)
+    
+    smooothScatterPar$x = data.frame(x = temp[, zmiennaOrg] , y = temp$reszty)
     do.call(smoothScatter, smooothScatterPar)
     grid(col=grey(0.5))
     lines(przewNPar, col=2, lwd=2, lty=1)
     lines(wielomian)
-
+    
     if( !is.null(folderWykresy) ){
       dev.off()
     }
-
+    
     linkTest[indeksGrupowania] = summary(lm(reszty ~ wielomian + I(wielomian^2), data=temp))$coefficients[3, 4]
     monotonicznosc[indeksGrupowania] = attributes(wielomian)$czyRosnaca
   }
-
-  if(is.null(grupowanie) | nrow(grupowanie) == 0){
-    tab = data.frame(            linkTest, monotonicznosc)
+  
+  if(is.null(grupowanie)){
+    tab = data.frame(linkTest, monotonicznosc)
   } else {
     tab = data.frame(grupowanie, linkTest, monotonicznosc)
   }
-  return(list(grupy = tab,
-              BIC = AIC(model, k=log(nrow(frame))),
+  return(list(grupy = tab, 
+              bic = AIC(model, k=log(nrow(frame))), 
               R2 = pseudoR2(model)))
 }
